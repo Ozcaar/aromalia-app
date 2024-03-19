765b23524781576a97c45c6e7eeba2478e5a04ec
Oscar Lopez <usrm0013@sellosmty.com>
2024-03-19 14:53:39 -0600

    Inicialiando el proyecto

---
+++ src/main/java/com/ozcaar/aromalia/AromaliaApplication.java
@@ -0,0 +1,13 @@
+package com.ozcaar.aromalia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AromaliaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AromaliaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ozcaar/aromalia/AromaliaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ozcaar.aromalia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AromaliaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
